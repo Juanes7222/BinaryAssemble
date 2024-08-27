46d57fb64d6a38845e3f202af5cccc3ee22da86f
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2024-08-26 21:50:51 -0500

    update

--- assemtobinary.py
+++ assemtobinary.py
@@ -12,7 +12,8 @@ def get_instructions_info():
         info = json.load(f)
     return info
 
-INFO: dict = get_instructions_info() 
+INFO: dict = get_instructions_info()
+
 def read_file(file):
     with open(file, "r", encoding="utf-8") as f:
         text_file = f.read()
@@ -27,7 +28,7 @@ def get_instruction(instruction: str):
 
 def type_instruction(instruction):
     for type_i, data in INFO.items():
-        if instruction in data.keys():
+        if instruction in data["inst"].keys():
             return type_i       
 
 def regular_expression(instrs: list, expression: str):
@@ -43,8 +44,8 @@ def tokenize(instruction, expression):
     else:
         raise ValueError(f"Invalid instruction: {instruction}")
 
-def decimal_to_binary(number: int, length=4):
-    binary = number.to_bytes(length=4, signed=True)
+def decimal_to_binary(number: int | str, length=4):
+    binary = int(number).to_bytes(length=4, signed=True)
     normal_binary = ''.join(format(byte, '08b') for byte in binary)
     # print(normal_binary)
     return normal_binary[-length:]
@@ -54,7 +55,7 @@ def get_number(reg):
 
 def registers(reg):
     num_reg = get_number(reg)
-    return decimal_to_binary(num_reg)
+    return decimal_to_binary(num_reg, 5)
 
 def r_instruction(instruction: dict, info):
     rd = instruction["rd"]
@@ -72,7 +73,7 @@ def r_instruction(instruction: dict, info):
     func7 = info["inst"][operation]["funct7"]
     opcode = info["opcode"]
     
-    binary = f"{func7}{rs2}{rs1}{func3}{rd}{opcode}"
+    binary = f"{func7}-{rs2}-{rs1}-{func3}-{rd}-{opcode}"
     
     return binary
 
@@ -87,8 +88,8 @@ def i_instruction(instruction: dict, info):
     imm = decimal_to_binary(imm, 12)
     operation = instruction["operation"]
     
-    opcode = info[operation]["opcode"]
-    funct3 = info[operation]["funct3"]
+    opcode = info["inst"][operation]["opcode"]
+    funct3 = info["inst"][operation]["funct3"]
     
     binary = f"{imm}{rs1}{funct3}{rd}{opcode}"
     if operation == "srai":
@@ -106,8 +107,8 @@ def s_instruction(instruction: dict, info):
     imm = decimal_to_binary(imm)
     
     operation = instruction["operation"]
-    funct3 = info[operation]["funct3"]
-    opcode = info["opcode"]
+    funct3 = info["inst"][operation]["funct3"]
+    opcode = info["inst"]["opcode"]
     binary = f"{imm[5:11]}{rs2}{funct3}{imm[0:4]}{opcode}"
     
     return binary
@@ -118,7 +119,7 @@ def u_instruction(instruction: dict, info):
     
     rd = instruction["rd"]
     operation = instruction["operation"]
-    opcode = info[operation]["opcode"]
+    opcode = info["inst"][operation]["opcode"]
     
     binary = f"{imm}{rd}{opcode}"
     
@@ -132,15 +133,15 @@ def b_instruction(instruction: dict, info, line):
     rs2 = instruction["rs2"]
     rs2 = registers(rs2)
     
-    imm = instruction["imm"]
-    imm = distance_label(imm, line)
-    imm = decimal_to_binary(imm, 12)
+    label = instruction["label"]
+    label = distance_label(label, line)
+    label = decimal_to_binary(label, 12)
     
     operation = instruction["operation"]
-    funct3 = info[operation]["funct3"]
+    funct3 = info["inst"][operation]["funct3"]
     opcode = info["opcode"]
     
-    binary = f"{imm[0]}{imm[2:7]}{rs2}{rs1}{funct3}{imm[8:11]}{imm[1]}{opcode}"
+    binary = f"{label[0]}{label[2:7]}{rs2}{rs1}{funct3}{label[8:11]}{label[1]}{opcode}"
     
     return binary
 
@@ -163,23 +164,45 @@ def get_info(instruction, line=None):
     inst = get_instruction(instruction)
     t_inst = type_instruction(inst)
     if t_inst:
-        return INFO[t_inst]
+        return INFO[t_inst], t_inst
     else:
         label = confirm_label(inst)
         if label:
             LABELS[label] = line+1
         else:
             raise ValueError(f"Invalid instruction: {instruction} --> Line: {line}")
+    return None, None
 
-# TODO: terminar la funcion del main y las instrucciones
+def instruction_manager(instruction, line):
+    info, t_inst = get_info(instruction, line)
+    if info:
+        re = regular_expression(info["inst"].keys(), info["re"])
+        token_instruction = tokenize(instruction, re)
+        if t_inst == "R":
+            binary = r_instruction(token_instruction, info)
+        elif t_inst == "I1" or t_inst == "I2":
+            binary = i_instruction(token_instruction, info)
+        elif t_inst == "S":
+            binary = s_instruction(token_instruction, info)
+        elif t_inst == "B":
+            binary = b_instruction(token_instruction, info, line)
+        elif t_inst == "U":
+            binary = u_instruction(token_instruction, info)
+        elif t_inst == "J":
+            binary = j_instruction(token_instruction, info)
+            
+        return binary
 
-def main(instruction):...
+def main(file):
+    instructions_file = read_file(file)
+    instructions = sep_lines(instructions_file)
+    binary_instructions = []
+    
+    for line, instruction in enumerate(instructions, 1):
+        binary = instruction_manager(instruction, line)
+        binary_instructions.append(binary)
+    return binary_instructions
 
-# info = INFO["I"]
-# expr = regular_expression(info, )
-# inst = tokenize("addi s3, s2, 12", info)
-# print(inst)
-# val = decimal_to_binary(-87, 12)
-# print(val)
-# print(len(val))
-# print(r_instruction(inst, info))
+print(main("./instruction.rsv"))
+            
+#TODO: corregir las expresiones regulares referentes a los imm() y terminar de validar
