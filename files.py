8e14a76420b2578fe0ace17e1573de4bfa34aeaa
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2024-09-30 09:12:03 -0500

    update

--- files.py
+++ files.py
@@ -1,5 +1,13 @@
 
 def push_info(file: str, info: list):
-    with open(file, "w+") as f:
-        for bin in info:
-            f.write(bin + "\n")
+    byte_data = bytearray()
+
+    for binary_str in info:
+        # Divide la cadena binaria en bloques de 8 bits
+        for i in range(0, len(binary_str), 8):
+            byte_chunk = binary_str[i:i+8]
+            byte_data.append(int(byte_chunk, 2))
+
+    
+    with open(file, "wb") as f:
+            f.write(byte_data)
